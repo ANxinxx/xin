742c471c0c348c787b833b9d955906ccffe1ff95
Anxinxx <3303338458@qq.com>
2024-12-15 14:49:26 +0800

    ok

---
+++ online-edu-master/src/main/java/xyz/refrain/onlineedu/OnlineEduApplication.java
@@ -0,0 +1,19 @@
+package xyz.refrain.onlineedu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Application Entrance
+ *
+ * @author Myles Yang
+ * @date 2021-05-01
+ */
+@SpringBootApplication
+public class OnlineEduApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineEduApplication.class, args);
+	}
+
+}
